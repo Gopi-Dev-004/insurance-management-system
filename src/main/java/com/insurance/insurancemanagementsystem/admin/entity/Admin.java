e9d6c0847090888295be0be186a133f54ab138d3
Gopi <mr.gopi0004@gmail.com>
2026-01-08 12:19:46 +0530

    feat: added repository for user

--- src/main/java/com/insurance/insurancemanagementsystem/admin/entity/Admin.java
+++ src/main/java/com/insurance/insurancemanagementsystem/admin/entity/Admin.java
@@ -39,6 +39,4 @@ public class Admin{
 
     private LocalDate doj;
 
-
-
 }
--- src/main/java/com/insurance/insurancemanagementsystem/vehicle/controller/CarController.java
+++ src/main/java/com/insurance/insurancemanagementsystem/vehicle/controller/CarController.java
@@ -1,9 +1,11 @@
 package com.insurance.insurancemanagementsystem.vehicle.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 public class CarController {
+
 }
